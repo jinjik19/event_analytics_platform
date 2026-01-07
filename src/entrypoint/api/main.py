972baa3fb676cf17f3919006a227073da69e54af
Evgeniy Krasnikov <e.krasnikov@runity.ru>
2026-01-07 13:38:04 +0300

    add middleware to app

--- src/entrypoint/api/main.py
+++ src/entrypoint/api/main.py
@@ -1,13 +1,18 @@
 from fastapi import APIRouter, FastAPI
 
+from entrypoint.api.lifespan import lifespan
+from entrypoint.api.middleware.logger import StructlogMiddleware
 from entrypoint.api.routers import healthz
 
 
 app = FastAPI(
     title="Event Analytics Platform",
+    lifespan=lifespan,
 )
 
-v1 = APIRouter(prefix="/api/v1")
-app.include_router(v1)
+app.add_middleware(StructlogMiddleware)
 
+v1 = APIRouter(prefix="/api/v1")
+
+app.include_router(v1)
 app.include_router(healthz.router)
