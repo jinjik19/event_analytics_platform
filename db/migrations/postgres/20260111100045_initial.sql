d7009d8cfd5ee1846b88ee8b2bb17cd0980451f6
Evgeniy Krasnikov <e.krasnikov@runity.ru>
2026-01-11 13:07:08 +0300

    fix migration

--- db/migrations/postgres/20260111100045_initial.sql
+++ db/migrations/postgres/20260111100045_initial.sql
@@ -3,7 +3,7 @@ CREATE TABLE "public"."project" (
   "project_id" uuid NOT NULL,
   "name" character varying(100) NOT NULL,
   "plan" character varying(10) NOT NULL,
-  "api_key" character varying(50) NULL,
+  "api_key" character varying(100) NULL,
   "created_at" timestamptz NOT NULL DEFAULT now(),
   PRIMARY KEY ("project_id"),
   CONSTRAINT "project_name_key" UNIQUE ("name")
--- db/schema/postgres/project_table.sql
+++ db/schema/postgres/project_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS project(
     project_id UUID PRIMARY KEY,
     name VARCHAR(100) NOT NULL UNIQUE,
     plan VARCHAR(10) NOT NULL,
-    api_key VARCHAR(50),
+    api_key VARCHAR(100),
     created_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 CREATE INDEX IF NOT EXISTS api_key_idx ON project (api_key);
