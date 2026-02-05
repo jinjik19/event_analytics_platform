c8af78199adda01a48b8123ad268206beb07ecfb
Evgeniy Krasnikov <e.krasnikov@runity.ru>
2026-02-05 12:07:28 +0700

    FEATURE: Update cache for redis

---
+++ src/infrastructure/di/providers/types.py
@@ -0,0 +1,12 @@
+from typing import TYPE_CHECKING, Any, NewType
+
+from redis.asyncio import Redis
+
+
+if TYPE_CHECKING:
+    RedisClient = Redis[Any]
+else:
+    RedisClient = Redis
+
+
+CacheRedis = NewType("CacheRedis", RedisClient)
